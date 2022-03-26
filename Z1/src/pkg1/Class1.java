1d45d58168fe606d9445a9d5e69be2a1e4f6b0e1
deepakchanana <deepakdeep1992@gmail.com>
2022-03-26 09:55:28 +0530

    pushing the code first time

---
+++ Z1/src/pkg1/Class1.java
@@ -0,0 +1,25 @@
+package pkg1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Class1
+{
+public static void main(String[] args) throws IOException
+{
+File f=new File("../Z1/dcdc.txt");
+FileWriter fw=new FileWriter(f,false);
+BufferedWriter b=new BufferedWriter(fw);
+b.newLine();
+b.write("third line of code");
+b.newLine();
+b.write("fourth line of code");
+b.close(); // for saving and closing the file
+
+}
+}
